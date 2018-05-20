48ef6b8450b29247a037f3d7e6cbb8c6a51c7d99
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2018-05-20 18:20:25 +0000

    Add basic new drawing form

---
+++ app/controllers/drawings_controller.rb
@@ -0,0 +1,23 @@
+class DrawingsController < ApplicationController
+    get '/drawings/new' do
+        erb :'drawings/new'
+    end
+    
+    post '/drawings' do
+        @drawing = Drawing.create(
+            background_color: params[:background_color],
+            background_effect: params[:background_effect],
+            text_color: params[:text_color],
+            outline_color: params[:outline_color],
+            outline_width: params[:outline_width],
+            content: params[:content],
+            title: params[:title]
+            )
+        redirect "/drawings/#{@drawing.id}"
+    end
+    
+    get '/drawings/:id' do
+        @drawing = Drawing.find(params[:id])
+        erb :'/drawings/show'
+    end
+end
