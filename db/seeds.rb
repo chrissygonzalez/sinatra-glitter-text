dc5cba76f4cdeea0d0a615097e347bb16341b1d2
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2018-05-28 21:46:36 +0000

    Add seed file

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+witchy = Theme.create(
+    name: "Witchy", 
+    font: "'Creepster' cursive", 
+    text_color: "red", 
+    text_shadow_color: "rgba(150, 150, 150, 1)", 
+    background_color: "#000", 
+    background_image: "url('images/witchy_pattern_by_apanicker-dbc3vzx.gif')", 
+    background_animation: ""
+)
+
+glitter = Theme.create(
+    name: "Glitter", 
+    font: "'Pacifico', cursive", 
+    text_color: "red", 
+    text_shadow_color: "rgba(255, 255, 255, 1)", 
+    background_color: "pink", 
+    background_image: "url('images/stars.gif'), radial-gradient(ellipse at top, pink, transparent), radial-gradient(ellipse at bottom, red, transparent)", 
+    background_animation: ""
+)
