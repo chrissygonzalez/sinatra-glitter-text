b6b6fe16cf1f36ffaa9f9cf235ffad77ea941807
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2018-05-29 00:08:08 +0000

    Add some styling, start login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,17 @@ class UsersController < ApplicationController
         if Helpers.logged_in?(session)
             redirect '/'
         else
-            redirect '/signup'
+            redirect '/login'
+        end
+    end
+    
+    post '/login' do
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/'
+        else
+            redirect '/login'
         end
     end
     
