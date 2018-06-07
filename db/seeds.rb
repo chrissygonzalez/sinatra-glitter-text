ee7738a0406b749540d09620f3f9c50a2ddf0295
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2018-06-07 12:04:58 +0000

    Update themes with new background and fonts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,29 @@
-witchy = Theme.create(
-    name: "Witchy", 
-    font: "'Creepster', cursive", 
+squiggly = Theme.create(
+    name: "Squiggly", 
+    font: "'Monoton', cursive", 
     text_color: "red", 
     text_shadow_color: "rgba(150, 150, 150, 1)", 
     background_color: "#000", 
-    background_image: "url('../images/witchy_pattern_by_apanicker-dbc3vzx.gif')", 
+    background_image: "url('../images/squiggles.gif')", 
     background_animation: ""
 )
 
-glitter = Theme.create(
-    name: "Glitter", 
+superstar = Theme.create(
+    name: "Superstar", 
     font: "'Pacifico', cursive", 
-    text_color: "red", 
-    text_shadow_color: "rgba(255, 255, 255, 1)", 
+    text_color: "#fff", 
+    text_shadow_color: "rgba(255, 94, 0, 1)", 
     background_color: "pink", 
-    background_image: "url('../images/stars.gif'), radial-gradient(ellipse at top, pink, transparent), radial-gradient(ellipse at bottom, red, transparent)", 
+    background_image: "url('../images/stars.gif')", 
+    background_animation: ""
+)
+
+stuff = Theme.create(
+    name: "Stuff", 
+    font: "'Creepster', cursive", 
+    text_color: "#03f", 
+    text_shadow_color: "rgba(150, 150, 150, 1)", 
+    background_color: "#000", 
+    background_image: "url('../images/items.gif')", 
     background_animation: ""
 )
